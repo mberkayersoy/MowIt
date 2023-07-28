da679042eaeb472fb43468b082c9ce3ef0e9d6ff
Berkan Yunus Marancı <berkanm.44@gmail.com>
2023-07-28 19:11:31 +0300

    UI and Effect manager added.

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 public class UIManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{ 
+    public static UIManager instance;
+    private int money=100;
+    [SerializeField] private TMPro.TextMeshProUGUI moneyText;
+
+    private void Awake()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetMoney(int value)
     {
-        
+        money=value;
+        moneyText.text = money.ToString();
+        moneyText.transform.DOScale(1.5f, 0.5f).SetEase(Ease.InOutBounce).OnComplete(() =>
+        {
+            moneyText.transform.DOScale(1f, 0.5f).SetEase(Ease.InOutBounce);
+        });
     }
 }
